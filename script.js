2bc89865197c5b839b556338678ce45bc962a18b
Berk Zengin <118611658+Berrrk@users.noreply.github.com>
2023-11-15 16:31:36 +0300

    Tik-tak!

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var targetDate = new Date("2023-11-30T00:00:00").getTime();
+
+    var x = setInterval(function () {
+        var now = new Date().getTime();
+        var distance = targetDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("countdown").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "Tik-ta...";
+        }
+    }, 1);
+});
